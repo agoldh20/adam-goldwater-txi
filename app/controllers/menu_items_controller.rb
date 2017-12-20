1fe321f9c71b6e938ffc5eace603f039c8630e0e
agoldh20 <dmgldwtr@gmail.com>
2017-12-20 15:08:56 -0600

    mvp menu controller, menu_item index view page

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,2 +1,39 @@
 class MenuItemsController < ApplicationController
+  def index
+    @menu_items = MenuItem.all
+  end
+
+  def new
+    @menu_item = MenuItem.new
+  end
+
+  def create
+    @menu_item = MenuItem.create(
+                                 name: params[:name],
+                                 description: params[:description],
+                                 price: params[:price])
+  end
+
+  def show
+    @menu_item = MenuItem.find(params[:id])
+  end
+
+  def edit
+    @menu_item = MenuItem.find(params[:id])
+  end
+
+  def update
+    menu_item = MenuItem.find(params[:id])
+    menu_item = MenuItem.update(
+                                name: params[:name],
+                                description: params[:description],
+                                price: params[:price])
+    redirect_to "/menu/#{menu_item.id}"
+  end
+
+  def destory
+    menu_item = MenuItem.find(params[:id])
+    menu_item.destroy
+    redirect_to "/menu"
+  end
 end
