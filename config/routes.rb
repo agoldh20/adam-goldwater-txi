b54279ee0c27968b8c521c2dd1cf09559aba4d46
agoldh20 <dmgldwtr@gmail.com>
2017-12-20 10:51:44 -0600

    built routes, seeded table model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  
+  # Menu
+  get '/menu_index' => 'menu_items#index'
+  get '/menu/new' => 'menu_items#new'
+  post '/menu' => 'menu_items#create'
+  get '/menu/:id' => 'menu_items#show'
+  get '/menu/:id/edit' => 'menu_items#edit'
+  patch '/menu/:id' => 'menu_items#update'
+  delete 'menu/:id' => 'menu_items#destroy'
+
+  # Table Sessions
+  get '/table' => 'sessions#new'
+  post '/table' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # Order Review
+  get '/review' => 'carted_drones#index'
+  post '/review' => 'carted_drones#create'
+  delete '/review/:id' => 'carted_drones#destroy'
+
+  #Order Sent
+  get '/order/new' => 'orders#new'
+  post '/order' => 'orders#create'
+  get '/order/:id' => 'orders#show'
+
 end
---
+++ db/old_seeds.rb
@@ -0,0 +1,7 @@
+# Table.create!([
+#               {name: "Table 1"},
+#               {name: "Table 2"},
+#               {name: "Table 3"},
+#               {name: "Table 4"},
+#               {name: "Table 5"}])
+#=========================================
