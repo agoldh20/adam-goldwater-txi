e13a3d8e9ecf6887be15eef146cffcc2610e7376
agoldh20 <a.goldh20@gmail.com>
2018-01-09 12:56:49 -0600

    fixed joint tables showing breakfast lunch and dinner

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -2,22 +2,23 @@ class MenuItemsController < ApplicationController
   def index
     @menu_items = MenuItem.all
 
-    @time_selected = params[:meal_time]
+    @time_selected = MealTime.find_by(name: params[:meal_time]).menu_items
+
+    app_items = Category.find_by(name: "App").menu_items
+    entree_items = Category.find_by(name: "Entree").menu_items
+    side_items = Category.find_by(name: "Side").menu_items
+    dessert_items = Category.find_by(name: "Dessert").menu_items
 
     if @time_selected
-      app_items = Category.find_by(name: "App").menu_items
-      entree_items = Category.find_by(name: "Entree").menu_items
-      side_items = Category.find_by(name: "Side").menu_items
-      dessert_items = Category.find_by(name: "Dessert").menu_items
       @apps = app_items & @time_selected
       @entrees = entree_items & @time_selected
       @sides = side_items & @time_selected
       @desserts = dessert_items & @time_selected
     else
-      @app = Category.find_by(name: "App").menu_items
-      @entree = Category.find_by(name: "Entree").menu_items
-      @side = Category.find_by(name: "Side").menu_items
-      @dessert = Category.find_by(name: "Dessert").menu_items
+      @apps = app_items
+      @entrees = entree_items
+      @sides = side_items
+      @desserts = side_items
     end
 
   end
